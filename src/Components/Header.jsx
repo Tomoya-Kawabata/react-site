a075c591930a37d2f2ae5e0909d192fafff6b9b9
tomoya-kawabata <tomoyaa4.24@gmail.com>
2022-09-12 19:37:05 +0900

    commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,13 +10,13 @@ const Header = () => {
       <nav>
         <ul>
           <li>
-            <Link to="react-site/">Home</Link>
+            <Link to="/react-site/">Home</Link>
           </li>
           <li>
-            <Link to="react-site/works">works</Link>
+            <Link to="/react-site/works">works</Link>
           </li>
           <li>
-            <Link to="react-site/contact">contact</Link>
+            <Link to="/react-site/contact">contact</Link>
           </li>
         </ul>
       </nav>
